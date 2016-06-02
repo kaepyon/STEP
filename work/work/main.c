32c3592f1bdc8a513e5659c751570f6c9cc9aec1
kaepyon <g1420526@is.ocha.ac.jp>
2016-06-02 23:33:46 +0900

    Initial Commit

---
+++ work/work/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  work
+//
+//  Created by 張嘉瑩 on 2016/06/02.
+//  Copyright (c) 2016年 Kaei Chou. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ work1/work1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+//辞書を並び替えたdict.textを作る
+	FILE *fp;
+	char *filename = "/usr/share/dict/words";
+	//char *output;
+	int N = 2000000;
+	char readline[N][256];
+	int i;
+
+	fp = fopen(filename,"r");
+	if(fp==NULL){
+		printf("Failed...\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for(i=0; i<N && fgets(readline[i],256,fp) != NULL; i++){
+		printf("readline[%d] = %s\n",i,readline[i]);
+	}
+
+	fclose(fp);
+	return 0;
+}
