ffca8ce7fb45f486bf3a9377a95b67013bfe7886
kaepyon <g1420526@is.ocha.ac.jp>
2016-06-05 20:56:39 +0900

    hw2finish

--- work1/work1.c
+++ work1/work1.c
@@ -4,11 +4,11 @@
 int main(){
 //辞書を並び替えたdict.textを作る
 	FILE *fp;
-	char *filename = "words.txt";
-	//char *output;
-	int N = 2000000;
-	char readline[N][256];
-	int i;
+	char *filename = "/usr/share/dict/words";
+
+	int N = 300000;
+	char *line;
+	line = (char*)malloc(256*N);
 
 	fp = fopen(filename,"r");
 	if(fp==NULL){
@@ -16,10 +16,10 @@ int main(){
 		exit(EXIT_FAILURE);
 	}
 
-	for(i=0; i<N && fgets(readline[i],256,fp) != NULL; i++){
-		printf("readline[%d] = %s\n",i,readline[i]);
-	}
+	fgets(line,256,fp);
+	printf("%s\n",line);
 
 	fclose(fp);
+	free(line);
 	return 0;
 }
