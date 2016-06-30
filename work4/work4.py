bb53b40e14ed335c6b7b6792abdacb657b6fb96d
kaepyon <g1420526@is.ocha.ac.jp>
2016-06-30 13:15:28 +0900

    hw5start

---
+++ work4/work4.py
@@ -0,0 +1,10 @@
+def makelst(node,val,lstlen):
+    lst = []
+    index = 0
+    while index < lstlen:
+        lst[index] = {"node"=node; "val"=val}
+        index += 1
+    return lst
+
+def initlst(node,val,lstlen):
+    
